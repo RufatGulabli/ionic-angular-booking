350aa1db3bf6db0affa133bb0af2a0d0041d7037
Rufat Gulabli <gulabli.rufat@gmail.com>
2019-07-22 14:06:26 +0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleMapApiKEY: 'AIzaSyCH8kq_KB4zEkUph-7oxo6m45_9ncy27YE',
-  googleWebApiKey: 'AIzaSyBdU92ZjcJUoLfqYedLagf_pDQP1V8uVLw'
+  googleMapApiKEY: '',
+  googleWebApiKey: ''
 };
