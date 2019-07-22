ca74d87d1c41b2f25f59b719ed48e05d7f1d7772
Rufat Gulabli <gulabli.rufat@gmail.com>
2019-07-22 14:06:59 +0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  googleMapApiKEY: 'AIzaSyCH8kq_KB4zEkUph-7oxo6m45_9ncy27YE',
-  googleWebApiKey: 'AIzaSyBdU92ZjcJUoLfqYedLagf_pDQP1V8uVLw'
+  googleMapApiKEY: '',
+  googleWebApiKey: ''
 };
 
 /*
